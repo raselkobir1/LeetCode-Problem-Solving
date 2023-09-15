5bf5fb727f4f1c497b0b72d1d3f94b950f245247
Rasel <USER@DESKTOP-LPSJO96>
2023-09-15 12:54:13 +0600

    ++ check ValidParentheses solution

---
+++ Solutions/ValidParentheses.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem.Solutions
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            Dictionary<char, char> bracketsMap = new Dictionary<char, char>{
+            {'{',  '}'},
+            {'(',  ')'},
+            {'[',  ']'},
+        };
+            Stack<char> openBrackets = new Stack<char>();
+
+            foreach (char bracket in s)
+            {
+                if (bracketsMap.ContainsKey(bracket))
+                {
+                    openBrackets.Push(bracket);
+                }
+                else
+                {
+                    if (openBrackets.Count == 0)
+                    {
+                        return false;
+                    }
+                    if (bracketsMap[openBrackets.Pop()] == bracket)
+                    {
+                        continue;
+                    };
+                    return false;
+                }
+            }
+            return openBrackets.Count == 0;
+        }
+    }
+}
