f66fddc49166f05778ca95c75df3d95e255c477c
Rasel <USER@DESKTOP-LPSJO96>
2023-09-18 16:46:59 +0600

    Palindrome Check problem solved

--- Program.cs
+++ Program.cs
@@ -2,7 +2,13 @@
 using LeetCodeProblem.Solutions;
 
 //Two sum
-int[] input = { 2, 7, 11, 15 };
-int target = 9;
-TwoSumSulotion twoSumSulotion = new TwoSumSulotion();
-twoSumSulotion.TwoSum(input, target);
+//int[] input = { 2, 7, 11, 15 };
+//int target = 9;
+//TwoSumSulotion twoSumSulotion = new TwoSumSulotion();
+//twoSumSulotion.TwoSum(input, target);
+
+int input = -121;
+PalindromeCheck palindromeCheck = new PalindromeCheck();
+var result = palindromeCheck.IsPalindrome(input);
+Console.WriteLine(result);
+Console.WriteLine();
---
+++ Solutions/PalindromeCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem.Solutions
+{
+    internal class PalindromeCheck 
+    {
+        public bool IsPalindrome(int x )
+        {
+            try
+            {
+                var LeftToRight = x;
+                var reverseData = x.ToString().ToCharArray().Reverse();
+                string result = string.Empty;
+
+                foreach (var item in reverseData)
+                {
+                    result += item; // Use += to concatenate characters to the result.
+                }
+
+                int RightToLeft = int.Parse(result); // Use int.Parse to convert the string to an integer.
+
+                if (LeftToRight == RightToLeft)
+                {
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;   
+            }
+
+        }
+    }
+}
