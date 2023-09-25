0948e7c611ae42120682befae14d2a8b534506cc
raselkobir1 <raselkobir29@gmail.com>
2023-09-25 08:32:46 +0600

    ++ mejority element 2nd part

--- Program.cs
+++ Program.cs
@@ -65,7 +65,7 @@ using LeetCodeProblem.Solutions;
 //    Console.WriteLine(i);   
 //}
 
-int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+int[] nums = { 2, 2, 1, 1, 3, 3, 3, 1, 2, 2 };
 //int[] nums = { 3, 2, 3 };
 _169_MajorityElementOfArray majority = new();
 int res = majority.MajorityElement(nums);
--- Solutions/169_MajorityElementOfArray.cs
+++ Solutions/169_MajorityElementOfArray.cs
@@ -1,26 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LeetCodeProblem.Solutions
+﻿namespace LeetCodeProblem.Solutions
 {
     public class _169_MajorityElementOfArray
     {
+        //int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
         public int MajorityElement(int[] nums)
         {
-            int count = 0;
-            int? candidate = null;
+            //int count = 0;
+            //int? candidate = null;
+
+            //foreach (int num in nums)
+            //{
+            //    if (count == 0) 
+            //        candidate = num;
+            //    count += (num == candidate) ? 1 : -1;
+            //}
+
+            //return candidate.Value; 
+
+            Dictionary<int, int> count = new Dictionary<int, int>();
 
             foreach (int num in nums)
             {
-                if (count == 0) 
-                    candidate = num;
-                count += (num == candidate) ? 1 : -1;
-            }
+                if (!count.ContainsKey(num))
+                {
+                    count[num] = 1;
+                }
+                else
+                {
+                    count[num]++;
+                }
 
-            return candidate.Value; 
+                // Check if the current element is the majority element
+                if (count[num] > nums.Length / 2)
+                {
+                    return num;
+                }
+            }
+            return -1;
         }
     }
 }
