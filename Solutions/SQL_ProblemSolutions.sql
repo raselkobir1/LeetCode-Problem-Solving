ebd9512c41176631a613246d60a52df4e038d0a1
Rasel <USER@DESKTOP-LPSJO96>
2023-09-25 10:56:41 +0600

    ++ 175 Combine two table

--- Solutions/SQL_ProblemSolutions.sql
+++ Solutions/SQL_ProblemSolutions.sql
@@ -3,3 +3,8 @@ SELECT Email
 FROM Person
 GROUP BY email
 HAVING COUNT(email) > 1;
+
+--#175 Combine two table
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a ON p.personId = a.personId;
