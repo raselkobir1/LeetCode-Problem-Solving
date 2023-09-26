e5c592580e11b5669d55994dcabb3ef871a6724e
Rasel <USER@DESKTOP-LPSJO96>
2023-09-26 17:59:28 +0600

    181 Employees-earning-more-than-their-managers

--- Solutions/SQL_ProblemSolutions.sql
+++ Solutions/SQL_ProblemSolutions.sql
@@ -8,3 +8,20 @@ HAVING COUNT(email) > 1;
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p
 LEFT JOIN Address a ON p.personId = a.personId;
+
+--#181 Employees-earning-more-than-their-managers
+Select e1.Name AS Employee 
+From Employee e1
+join Employee e2 on e1.managerId = e2.id 
+Where e1.salary > e2.salary
+
+--way 2
+SELECT e1.name AS Employee
+FROM Employee e1
+WHERE e1.salary > (
+    SELECT MAX(e2.salary)
+    FROM Employee e2
+    WHERE e2.id = e1.managerId
+);
+
+
