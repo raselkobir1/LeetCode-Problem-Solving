6a081aad1a784e8a575d10888da0c51e2334c699
raselkobir1 <raselkobir29@gmail.com>
2023-10-10 22:40:58 +0600

    ++ ContainsDuplicates

---
+++ Solutions/ContainsDuplicate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem.Solutions
+{
+    internal static class ContainsDuplicate
+    {
+        public static void ContainsDuplicates()
+        {
+            int[] input = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+            var res =  ContainsDuplicatee(input);
+            Console.WriteLine(res);
+        }
+        public static bool ContainsDuplicatee(int[] nums)
+        {
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
