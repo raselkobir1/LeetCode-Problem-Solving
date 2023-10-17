0e7a481a8706efcd51fe15e032a91337ed10b6c4
raselkobir1 <raselkobir29@gmail.com>
2023-10-17 22:51:50 +0600

    ++ AddStrings

---
+++ Solutions/AddStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblem.Solutions
+{
+    internal class AddStrings
+    {
+        public class Solution
+        {
+            public string AddStrings(string num1, string num2)
+            {
+                BigInteger x = BigInteger.Parse(num1) + BigInteger.Parse(num2);
+                return x.ToString();
+            }
+        }
+    }
+}
