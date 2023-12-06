f232eec9260156cb107404be6b6ea4fb67ce9e71
Rasel <md.rasel@technonext.com>
2023-12-06 23:02:47 +0600

    1327_list-the-products-ordered-in-a-period

--- Solutions/SQL_ProblemSolutions.sql
+++ Solutions/SQL_ProblemSolutions.sql
@@ -38,3 +38,12 @@ LEFT JOIN Examinations e ON s.student_id = e.student_id AND sub.subject_name = e
 GROUP BY s.student_id, s.student_name, sub.subject_name
 ORDER BY s.student_id, sub.subject_name;
 
+/* Write your T-SQL query statement below */
+
+SELECT p.product_name, SUM(o.unit) AS unit
+FROM Products p
+INNER JOIN Orders o ON p.product_id = o.product_id
+WHERE  o.order_date between '2020-02-01'and '2020-02-29'
+GROUP BY p.product_id, p.product_name
+HAVING SUM(o.unit) >= 100;
+
